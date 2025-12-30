46a4d30a8b51496ea58faeca8b956f4d302271a3
Rituraj <riturajhmr@gmail.com>
2025-12-30 16:56:43 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ShowDetails from "./pages/ShowDetails";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
@@ -18,8 +12,6 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
 );
 
 export default App;
